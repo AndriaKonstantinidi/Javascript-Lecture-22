022bee89284e38777e968ed72722df949d7c696f
Student <student@itstep.ge>
2022-12-06 19:01:28 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// let h3 = document.querySelector('h3');
+// let btnRed = document.querySelector('.btnRed');
+// let btnGreen = document.querySelector('.btnGreen');
+
+// btnRed.addEventListener('click',function(){
+//     h3.classList.add('red');
+//     h3.classList.remove('green')
+// })
+
+// btnGreen.addEventListener('click',function(){
+//     h3.classList.add('green');
+//     h3.classList.remove('red');
+// })
+
+// let btn = document.querySelector('button');
+// let box = document.querySelector('div');
+// btn.addEventListener('click',function(){
+//     box.classList.toggle('hideShow')
+// })
